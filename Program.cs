5456c101d8c17ea24fa5a8cbcf3c9be6d8a7007d
USEB - ARS - Matheus Leandro <matheus.leandro@adventistas.org>
2022-10-21 15:54:14 -0300

    Primeiro programa... Imprimindo na tela 'hello world'.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System; // Foi usado o comando "dotnet new console" para criar este projeto c#.
+
+namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); // comando para printar na tela.
+        }
+    }
+}
