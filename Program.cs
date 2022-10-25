9be5a9ab436b1760952bcbc36324e2418334ee5a
matheus <matheus.leandroyt@gmail.com>
2022-10-24 21:18:44 -0300

    Criando uma variável pela primeira vez

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace FirstApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!"); // comando para printar na tela com quebra de linha.
-            Console.Write("Olá Mundo"); // Comando para printar na tela sem quebra de linha.            
+            Console.Write("Olá Mundo"); // Comando para printar na tela sem quebra de linha.    
+            double x = 10.3543; // Criando uma variável do tipo double cujo nome é "x" e valor armazenado nessa variável é "10.3543".
+            Console.Writeline(x);  //Imprimindo uma variável.      
         }
     }
 }
