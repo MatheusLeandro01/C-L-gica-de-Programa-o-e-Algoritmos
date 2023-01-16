40c14673fb09f7f7d45b64e580dd2cda5e0663ec
matheus.leandroyt@gmail.com <matheus.leandroyt@gmail.com>
2023-01-16 20:24:39 -0300

    Primeiro simples commit

---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; //First Commit;
+
+namespace FirstCommit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Commit");
+        }
+    }
+}
