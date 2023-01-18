6beb18b1137923ca5911795be1fb3f90f14e36c8
Matheus Leandro <matheus.leandroyt@gmail.com>
2023-01-17 21:14:12 -0300

    Enumeração e Composição

---
+++ Enumeracao/Enumerate/Enumerate/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Enumerate.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 8,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
