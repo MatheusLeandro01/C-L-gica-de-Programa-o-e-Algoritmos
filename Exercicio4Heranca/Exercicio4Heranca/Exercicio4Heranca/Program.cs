f31cd827fd4b725af574ef4778260532102d1aa7
matheus.leandroyt@gmail.com <matheus.leandroyt@gmail.com>
2023-01-27 15:32:42 -0300

    Upcasting e Downcasting

--- Exercicio4Heranca/Exercicio4Heranca/Exercicio4Heranca/Program.cs
+++ Exercicio4Heranca/Exercicio4Heranca/Exercicio4Heranca/Program.cs
@@ -6,7 +6,27 @@ namespace Exercicio4Heranca
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
+            Account acc = new Account(1001, "Alex", 0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0, 500);
+
+            //Upcasting
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200);
+            Account acc3 = new SavingAccount(1004, "Anna", 0.0, 1.0);
+            // acc2.Loan(); -> Não é possível porque acc2 é do tipo account e esse método pertence a classe business account.
+
+            //Downcasting
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            
+            if (acc3 is SavingAccount)
+            {
+                //SavingAccount acc5 = (SavingAccount)acc3;
+                SavingAccount acc5 = acc3 as SavingAccount;               
+                Console.WriteLine("Update");
+            }
+            
+            
         }
     }
 }
