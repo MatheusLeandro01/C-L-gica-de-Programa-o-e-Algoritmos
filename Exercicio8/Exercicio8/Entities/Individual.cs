7dc789cb24d0458d8416eafbd04c9c2cf11edb57
matheus.leandroyt@gmail.com <matheus.leandroyt@gmail.com>
2023-01-31 14:23:51 -0300

    Exercicio sobre Classes e métodos abstratos

---
+++ Exercicio8/Exercicio8/Entities/Individual.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio8.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditure { get; set; } = 0.00;
+
+        public Individual() { }
+
+        public Individual(string name, double anualIncome, double healthExpenditure)
+            : base(name, anualIncome)
+        {
+            HealthExpenditure = healthExpenditure;
+        }
+        public override double Tax()
+        {
+
+            if (AnualIncome < 20000.0)
+            {
+                return AnualIncome * 0.15 - HealthExpenditure * 0.5;
+            }
+            else
+            {
+                return AnualIncome * 0.25 - HealthExpenditure * 0.5;
+            }
+        }
+    }
+}
---
+++ Exercicio8/Exercicio8/Entities/TaxPayer.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicio8.Entities
+{
+    abstract class TaxPayer //Classe Abstrata
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public TaxPayer() { }
+
+        protected TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax(); //Método abstrato
+    }
+}
