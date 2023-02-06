aa624fcc07e2d32b8b537e45fcc4d0b3fea6008c
matheus.leandroyt@gmail.com <matheus.leandroyt@gmail.com>
2023-02-06 14:51:19 -0300

    Utilizando as Classes FileStream e Stream Reader

---
+++ Exercicio12/Exercicio12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Exercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+
+            FileStream fs = null; //stream genérica para acessar recurso de entrada e saída;
+            StreamReader sr = null; //stream particular otimazada para o que eu quero;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open); //aqui eu informo o caminho do arquivo e o que eu quero fazer com ele
+                //Filestream trabalha em baixo nível
+                sr = new StreamReader(fs); //preciso do streamReader para transformar em string
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine(); //transformando o conteúdo de FileStream em string
+                    Console.WriteLine(line);
+                }                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
