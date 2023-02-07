cea4132cee2543b02b53d9bc5eb9a06fd046e8d1
matheus.leandroyt@gmail.com <matheus.leandroyt@gmail.com>
2023-02-07 19:24:32 -0300

    aprendo a utilizar o streamwriter 'manipulação de arquivos'.

---
+++ Exercicio13/Exercicio13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Exercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string origem = @"c:\temp\file1.txt";
+            string destino = @"c:\temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(origem);
+
+                using (StreamWriter sw = File.AppendText(destino))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
