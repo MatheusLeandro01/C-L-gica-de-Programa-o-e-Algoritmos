c6e6a85f90cca366547c8b8e812545de8cfbb36e
matheus.leandroyt@gmail.com <matheus.leandroyt@gmail.com>
2023-02-07 20:05:22 -0300

    Utilizando a classe 'Path'

---
+++ Exercicio15/Exercicio15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Exercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminho = @"c:\temp\myfolder\test1.txt";
+
+            try
+            {
+                Console.WriteLine("Directory Separetor Char: " + Path.DirectorySeparatorChar);
+                Console.WriteLine("Path Separetor: " + Path.PathSeparator);
+                Console.WriteLine("Get Directory Name: " + Path.GetDirectoryName(caminho)); //informa o diretório
+                Console.WriteLine("Get File Name: " + Path.GetFileName(caminho));
+                Console.WriteLine("Get Extension: " + Path.GetExtension(caminho));
+                Console.WriteLine("Get File Name Without Extension: " + Path.GetFileNameWithoutExtension(caminho));
+                Console.WriteLine("Get Full Path: " + Path.GetFullPath(caminho));
+                Console.WriteLine("Get Temp Path: " + Path.GetTempPath());
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
