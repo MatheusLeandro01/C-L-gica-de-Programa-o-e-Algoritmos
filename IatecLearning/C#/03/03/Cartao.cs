21c23262bb0b211507fdc1b1c7574f669821c76f
Matheus Leandro <matheus.leandroyt@gmail.com>
2023-05-16 16:03:27 -0300

    Classes ricas

---
+++ IatecLearning/C#/03/03/Cartao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto
+{
+    internal class Cartao //classe cartão
+    {
+        public string NomeProprietario { get; private set; } //propriedades da classe cartão
+        public string Numero { get; private set; }
+        public int Digito { get; private set; }
+        public decimal Saldo { get; private set; }
+        public decimal Limite { get; private set; }
+        public bool Ativo { get; private set; }
+        public DateTime Validade { get; private set; }    
+
+        
+        public Cartao() { }
+
+        public Cartao(string nomeProprietario, string numero, int digito, decimal saldo, decimal limite, bool ativo, DateTime validade)
+        {
+            NomeProprietario = nomeProprietario;
+            Numero = numero;
+            Digito = digito;
+            Saldo = saldo;
+            Limite = limite;
+            Ativo = ativo;
+            Validade = validade;            
+        }
+
+        public void AlterarNomeProprietario(string nome)
+        {
+            NomeProprietario = nome;
+        }
+        public void Debitar(decimal valor)
+        {
+            Saldo += valor;
+        }
+    }
+}
---
+++ IatecLearning/C#/03/03/Program.cs
@@ -0,0 +1,7 @@
+﻿using Projeto;
+
+
+Cartao cartao = new Cartao("Matheus", "123456789", 5, 1000M, 1500M, true, DateTime.Now.AddYears(5));
+
+cartao.AlterarNomeProprietario("Victor");
+cartao.Debitar(100M);
