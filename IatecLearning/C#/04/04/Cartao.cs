37466e27dd893a7320b7741c862241ae1ae5422b
Matheus Leandro <matheus.leandroyt@gmail.com>
2023-05-16 14:25:50 -0300

    Classes anêmicas e as suas desvantagens

---
+++ IatecLearning/C#/04/04/Cartao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto
+{
+    internal class Cartao //classe cartão
+    {
+        public string NomeProprietario { get; set; } //propriedades da classe cartão
+        public string Numero { get; set; }
+        public int Digito { get; set; }
+        public decimal Saldo { get; set; }
+        public decimal Limite { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime Validade { get; set; }
+        public NomeBanco Banco { get; set; }
+        public TipoCartao Tipo { get; set; }
+
+        public string ObterNumeroCompleto()
+        {
+            return $"{Numero}-{Digito}";
+        }
+    }
+}
---
+++ IatecLearning/C#/04/04/Program.cs
@@ -0,0 +1,10 @@
+﻿using Projeto;
+
+//Na forma anêmica conseguimos manipular qualquer valor
+
+Cartao cartao = new Cartao();
+
+cartao.Ativo = false;
+cartao.NomeProprietario = "Matheus"; // não é legal fazer adições ou alterações dessa forma
+
+cartao.NomeProprietario = "Leandro"; // alterando o nome do propietário
