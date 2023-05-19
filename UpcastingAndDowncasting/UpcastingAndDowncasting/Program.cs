da83e371761106313727ee19c2939587f9c04a49
Matheus Leandro <matheus.leandroyt@gmail.com>
2023-05-19 12:15:05 -0300

    Aprendendo os conceitos de Upcasting e Downcasting

---
+++ UpcastingAndDowncasting/UpcastingAndDowncasting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Praticando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pessoa pessoa = new PessoaFisica(); //Upcasting
+
+            Pessoa pessoa = new Pessoa();
+            PessoaFisica pessoaFisica = new PessoaFisica();
+            PessoaJuridica pessoaJuridica = new PessoaJuridica();
+
+            pessoaFisica = (PessoaFisica)pessoa;//casting(conversão explícita) -> downcasting -> propriedades de pessoaFisica poderão ser perdidas           
+        }
+        class Pessoa
+        {
+            public string Nome { get; set; }
+        }
+
+        class PessoaFisica : Pessoa
+        {
+            public string Cpf { get; set; }
+        }
+        class PessoaJuridica : Pessoa
+        {
+            public string Cnpj { get; set; }
+        }
+    }
+}
